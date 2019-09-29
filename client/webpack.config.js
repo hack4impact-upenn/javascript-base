329acd3bf4733621b65db6a94783fed5afd0628f
Katie Jiang <kajiang@seas.upenn.edu>
2019-09-29 01:47:04 -0400

    Add Babel to client

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,6 +1,18 @@
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js']
+  },
   output: {
     path: __dirname + '/dist',
     publicPath: '/',
