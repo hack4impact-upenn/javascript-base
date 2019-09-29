722a1ddf256207346bb6ccc85a329a622424098c
Katie Jiang <kajiang@seas.upenn.edu>
2019-09-29 01:49:27 -0400

    Add Babel + Webpack (#2)
    * Add Babel to server
    
    * Add Webpack to client, separate from server
    
    * Fix server/.babelrc
    
    * Add Babel to client
    
    * Update README

---
+++ client/webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist',
+    watchContentBase: true,
+    port: 3000
+  }
+};
