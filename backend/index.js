f0710e16e0b28e56eecf15153e946e2fbd74d323
anniesu1 <anniesumail@gmail.com>
2019-10-05 19:44:16 -0400

    add mongodb setup to ssr-server

--- backend/index.js
+++ backend/index.js
@@ -1,31 +1,3 @@
-const express = require("express");
-const app = express();
-
-// Connect to mongodb using mongoose
-require("dotenv").config();
-const mongoose = require("mongoose");
-mongoose.connect(process.env.MONGODB_URI);
-
-// Extract User model
-const models = require("./models/models");
-const User = models.User;
-
-const eraseDatabaseOnSync = true;
-var db = mongoose.connection;
-db.on("error", console.error.bind(console, "Connection error:"));
-db.once("open", async function() {
-  // We're connected!
-  console.log("Connected to database");
-
-  // Delete everything from the database
-  if (eraseDatabaseOnSync) {
-    await Promise.all([User.deleteMany({})]);
-  }
-
-  // Seed the database
-  createFakeUsers();
-});
-
 async function createFakeUsers() {
   let annieUser = new User({
     firstName: "annie",
@@ -54,11 +26,3 @@ async function createFakeUsers() {
   await katieUser.save();
   console.log("Added katie");
 }
-
-const port = 3000;
-app.listen(port, err => {
-  if (err) {
-    return console.error(err);
-  }
-  console.log(`Server is listening on ${port}`);
-});
