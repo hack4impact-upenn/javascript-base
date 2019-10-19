b66ca8a4ed2e1e5f983cf4bc32c00c85603ba6d0
anniesu1 <anniesumail@gmail.com>
2019-10-18 21:52:45 -0400

    made style fixes

--- src/ssr-server.js
+++ src/ssr-server.js
@@ -8,11 +8,6 @@ const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-// import { EmailService } from "./services/email-service";
-//let mailer = new EmailService("Subject", "Body", "test@example.com");
-
-const EmailService = require("./services/email-service").default;
-
 // Connect to mongodb using mongoose
 require("dotenv").config();
 const mongoose = require("mongoose");
@@ -49,15 +44,6 @@ app
       if (err) throw err;
       console.log("> Ready on http://localhost:3000");
     });
-
-    // -----TODO: delete later (sorry)
-    let mailer = new EmailService("Subject", "Body", "test@example.com");
-    mailer
-      .sendTo(["hack4impact@gmail.com"])
-      .then(response => console.log(response.statusCode))
-      .catch(err => console.log(err.message));
-    return;
-    // -----End of delete
   })
   .catch(ex => {
     console.error(ex.stack);
