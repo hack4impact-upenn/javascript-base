bad83e3a224834a2d33f6a691fc02a89b923af08
Gautam Narayan <gnarayan@seas.upenn.edu>
2019-10-24 15:40:58 -0400

    Fake Data Script and User Roles

--- src/backend/fakedata.ts
+++ src/backend/fakedata.ts
@@ -14,7 +14,7 @@ const User = models.User;
 const eraseDatabaseOnSync = true;
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "Connection error:"));
-db.once("open", async function() {
+db.once("open", async function() {  
   // We're connected!
   console.log("Connected to database");
 
