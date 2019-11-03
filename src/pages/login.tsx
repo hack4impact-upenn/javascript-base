2ea123d0806272f1b999a0023eaafc5663ee8ef5
Jared Asch <jaredasch1@gmail.com>
2019-11-03 01:31:41 -0500

    form styling and validation

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-
-import { AppBar, Button, Grid, TextField } from '@material-ui/core';
+import { AppBar, Button, Grid, TextField, FormControlLabel, Checkbox, Typography } from '@material-ui/core';
 import { ValidatorForm, TextValidator} from 'react-material-ui-form-validator';
 
 import client from "../components/apollo"
@@ -9,15 +8,17 @@ import { gql } from "apollo-boost";
 
 
 interface LoginPageState {
-  email: string
-  password: string
+  email: string,
+  password: string,
+  error: string
 }
 
 class LoginPage extends React.Component<{}, LoginPageState> {
 
   state: LoginPageState = {
     email: "",
-    password: ""
+    password: "",
+    error: ""
   }
 
   private LOGIN_QUERY = gql`
@@ -42,7 +43,9 @@ class LoginPage extends React.Component<{}, LoginPageState> {
         password: this.state.password
       }
     }).then((data : any) => {
-      console.log(data)
+      // TODO : Redirect to different page
+    }).catch((error : any) => {
+      this.setState({...this.state, error: error.graphQLErrors[0].message})
     })
   }
 
@@ -51,23 +54,38 @@ class LoginPage extends React.Component<{}, LoginPageState> {
       <ApolloProvider client={client}>
         <div>
           <AppBar title="Login" />
-          <Grid container spacing={0} direction="column" alignItems="center" justify="center">
-            <ValidatorForm onSubmit = {this.handleLogin}>
+
+          <Grid container justify="center">
+            <Grid item xs = {10} sm = {6} md = {4} lg = {4}>
+            <ValidatorForm onSubmit = {this.handleLogin} style = {{ 
+                padding: "20px", 
+                marginTop: "40px",
+                backgroundColor: "white",
+                borderBottom: "1px solid rgba(0, 0, 0, 0.2)",
+                boxShadow: "0 1px 5px rgba(0, 0, 0, 0.15)" }}>
+              <Typography variant="h5">Log In</Typography>
               <TextValidator type="text" placeholder="Enter your Email" label="Email" name = "email"
                              onChange={ this.handleEmailChange }
                              validators = {['required']}
                              errorMessages = {['Required']}
                              value = { this.state.email }
+                             style = {{ margin: "10px 0" }}
+                             fullWidth
                              />
               <br />
               <TextValidator type="password" placeholder="Enter your Password" label="Password" name="password"
                              onChange={ this.handlePasswordChange }
                              validators = {['required']}
                              errorMessages = {['Required']}
-                             value = { this.state.password }/>
+                             value = { this.state.password }
+                             style = {{ margin: "10px 0" }}
+                             error = { this.state.error != "" }
+                             helperText = {this.state.error == "" ? "" : this.state.error}
+                             fullWidth/>
               <br />
-              <Button type = "submit">Log In</Button>
+              <Button style = {{ margin: "10px 0" }} variant = "contained" color = "primary" type = "submit">Log In</Button>
             </ValidatorForm>
+            </Grid>
           </Grid>
         </div>
       </ApolloProvider>
