6d0742c9512365245264fd521d3d38e018bb0d7a
Jared Asch <jaredasch1@gmail.com>
2019-11-05 14:19:52 -0500

    File uploading to AWS

--- src/backend/modules/file/resolvers.js
+++ src/backend/modules/file/resolvers.js
@@ -2,10 +2,13 @@ import AWS from "aws-sdk"
 import { File, User } from "../../models"
 import { AuthenticationError } from "apollo-server";
 
-// const s3 = new AWS.S3({
-//   accessKey: process.env.AWS_ACCESS_KEY,
-//   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-// })
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+})
 
 const resolvers = {
   Mutation : {
@@ -24,7 +27,12 @@ const resolvers = {
 
       newFile.save();
       currentUser.save();
-      // TODO : Save to AWS
+      
+      const params = {Bucket: process.env.AWS_S3_BUCKET, Key: newFile.id, Body: stream}
+      s3.upload(params, (error, data) => {
+        console.log(data)
+        console.log(error)
+      })
       return true;
     }
   }
