81aced28e0392b4a8c5a3a60437ee536b2001323
Katie Jiang <katiesjiang@gmail.com>
2019-12-10 15:16:10 -0500

    Prettify /files page, clean up index (#33)
    * Clean up files page, add to navbar
    
    * Clean up index + move graphql to navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,10 @@
 import React from "react";
-import NextLink from "next/link";
 
 import Navbar from "../components/Navbar";
 
 const Index = () => (
   <React.Fragment>
     <Navbar />
-    <ul>
-      <li><NextLink href="/api">GraphQL</NextLink></li>
-      <li><NextLink href="/files">My Files</NextLink></li>
-      <li><NextLink href="/upload">Upload</NextLink></li>
-      <li><NextLink href="/profile">Profile</NextLink></li>
-      <li><NextLink href="/admin">Admin</NextLink></li>
-    </ul>
   </React.Fragment>
 );
 
