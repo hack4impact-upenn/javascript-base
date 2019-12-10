3b45333668bfa228a51025ab113fd77441ea5aa5
stephanieyshi <stephanie.y.shi@gmail.com>
2019-12-10 15:23:48 -0500

    fix indenting

--- src/pages/account/password.tsx
+++ src/pages/account/password.tsx
@@ -1,22 +1,22 @@
 import React from "react";
- import { AppBar,Grid,  } from '@material-ui/core';
+import { AppBar, Grid, } from '@material-ui/core';
 
-  import ChangePasswordForm from "../../components/ChangePasswordForm"
+import ChangePasswordForm from "../../components/ChangePasswordForm"
 
-  class AccountPage extends React.Component<{}, {}> {
+class AccountPage extends React.Component<{}, {}> {
 
-    public render() {
-     return (
-         <React.Fragment>
-           <AppBar title="Account" />
-           <Grid container justify="center">
-             <Grid item xs = {10} sm = {6} md = {4} lg = {4}>
-               <ChangePasswordForm></ChangePasswordForm>
-             </Grid>
-           </Grid>
-         </React.Fragment>
-     );
-   }
- }
+  public render() {
+    return (
+      <React.Fragment>
+        <AppBar title="Account" />
+        <Grid container justify="center">
+          <Grid item xs={10} sm={6} md={4} lg={4}>
+            <ChangePasswordForm></ChangePasswordForm>
+          </Grid>
+        </Grid>
+      </React.Fragment>
+    );
+  }
+}
 
-  export default AccountPage;
+export default AccountPage;
